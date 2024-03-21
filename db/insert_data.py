4f92644f8064682fdec1ccd8612b904a5d3066bf
ilyadev750 <petrovich76007600kz@gmail.com>
2024-03-21 15:08:58 +0100

    adding info to db

--- db/insert_data.py
+++ db/insert_data.py
@@ -1,15 +1,14 @@
 from sqlalchemy.orm import Session
-from models import Category
+from models import Category, MainShelve, Good, MinorShelve
 from engine import engine
 
 
 with Session(engine) as session:
-    laptop = Category(category="Ноутбук",)
-    monitor = Category(category="Монитор",)
-    system_block = Category(category="Системный блок")
-    clocks = Category(category="Часы")
-    microphone = Category(category="Микрофон")
-
-    # session.add_all([laptop, monitor, system_block, clocks, microphone])
-    session.add_all([laptop])
+    m_shelve_1 = MinorShelve(name='Б', good_id=2)
+    m_shelve_2 = MinorShelve(name='5', good_id=2)
+    m_shelve_3 = MinorShelve(name='А', good_id=4)
+    m_shelve_4 = MinorShelve(name='10', good_id=4)
+    m_shelve_5 = MinorShelve(name='ж', good_id=7)
+    
+    session.add_all([m_shelve_1, m_shelve_2, m_shelve_3, m_shelve_4, m_shelve_5])
     session.commit()
--- db/models.py
+++ db/models.py
@@ -38,13 +38,12 @@ class MainShelve(Base):
     __table_args__ = (UniqueConstraint('category_id', name='unique_category'),)
 
     category: Mapped["Category"] = relationship(back_populates="main_shelve")
-    minor_shelves: Mapped[List["MinorShelve"]] = relationship(back_populates="main_shelve")
-
+    
 
 class Good(Base):
 
     __tablename__ = "goods"
-
+    
     id: Mapped[int] = mapped_column(primary_key=True)
     model_name: Mapped[str] = mapped_column(String(50))
     category_id: Mapped[int] = mapped_column(
@@ -54,6 +53,7 @@ class Good(Base):
     __table_args__ = (UniqueConstraint('model_name', name='unique_model'),)
     
     category: Mapped["Category"] = relationship(back_populates="goods")
+    minor_shelves: Mapped[List["MinorShelve"]] = relationship(back_populates="good")
     
 
 class MinorShelve(Base):
@@ -62,13 +62,12 @@ class MinorShelve(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(50))
-    main_shelve_id: Mapped[int] = mapped_column(
-        ForeignKey("main_shelves.id", onupdate="CASCADE", ondelete="CASCADE")
+    good_id: Mapped[int] = mapped_column(
+        ForeignKey("goods.id", onupdate="CASCADE", ondelete="CASCADE")
         )
     
     __table_args__ = (
-        UniqueConstraint('name', 'main_shelve_id', name='unique_minor_shelve_for_main_shelve'),
-        CheckConstraint(name != main_shelve_id.name, name='not_equal_main_shelve')
+        UniqueConstraint('name', 'good_id', name='unique_minor_shelve_for_good'),
         )
     
-    main_shelve: Mapped["MainShelve"] = relationship(back_populates="minor_shelves")
+    good: Mapped["Good"] = relationship(back_populates="minor_shelves")
